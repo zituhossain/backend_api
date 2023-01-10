34e4873af0a83e3468e1146d62316a5a49c48c7d
Mr.Al-Amin Hossain <jomaddaralamin69@gmail.com>
2023-01-10 23:56:24 +0600

    env update

--- models/index.js
+++ models/index.js
@@ -6,7 +6,8 @@ const Sequelize = require('sequelize');
 const process = require('process');
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.json')[env];
+let js_file_name = `config_${env}.json`;
+const config = require(__dirname + '/../config/'+js_file_name)[env];
 const db = {};
 
 let sequelize;
