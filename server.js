3a9de597edf47f0289eeff3758c6b67907b7ec3d
Mr.Al-Amin Hossain <jomaddaralamin69@gmail.com>
2023-01-12 13:05:13 +0600

    Update server.js

--- server.js
+++ server.js
@@ -38,7 +38,7 @@ app.use(cookieParser());
 
 var allowlist = ['http://192.168.0.102:3000', 'https://web.zvgy.com']
 //To allow cross-origin requests
-app.use(cors({origin:allowlist}));
+app.use(cors({origin:allowlist,methods: ['GET','POST','DELETE','UPDATE','PUT','PATCH']}));
 app.options('*', cors());
 // add multer middleware
 app.disable('x-powered-by');
