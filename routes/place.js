d3ef6dff912d7dd40622b368eea6a25373ad550c
Sonjoy Biswas <sonjoywc@gmail.com>
2023-01-26 22:59:22 +0600

    places page

--- routes/place.js
+++ routes/place.js
@@ -10,6 +10,9 @@ router.post('/create_place',auth, PlaceController.createPlace);
 router.get('/delete_place/:id',auth, PlaceController.deleteplacebyid);
 router.post('/update_place/:id',auth, PlaceController.updatePlace);
 router.get('/districtmap/:id',auth, PlaceController.getDistrictmap);
+router.get('/get_district/:id',auth, PlaceController.getDistrict);
+router.get('/get_division/:id',auth, PlaceController.getDivision);
+router.get('/get_district_by_division_id/:id',auth, PlaceController.getDistrictByDivision);
 
 
 module.exports = router;
