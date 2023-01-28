a5911976cf891a96c692459e47b8fcd2bc98c21d
Mahidul Moon <49182474+mahidulmoon@users.noreply.github.com>
2023-01-28 20:48:16 +0600

    bishal kam

--- controllers/PlaceController.js
+++ controllers/PlaceController.js
@@ -7,9 +7,19 @@ exports.getallPlace = async (req, res) => {
     try {
         const token = req.headers.authorization.split(' ')[1];
         let roleByplace = await checkUserRoleByPlace(token)
-        console.log(roleByplace)
+        // console.log(roleByplace)
+        let arr = []
+        if(roleByplace.district.length > 0){
+            arr.push({district_id: roleByplace.district})
+        }else if(roleByplace.division.length > 0){
+            arr.push({division_id: roleByplace.division})
+        }else if(roleByplace.place.length > 0){
+            arr.push({id: roleByplace.place})
+        }
+        // console.log(arr)
         const place_data = await Place.findAll({
-            include: [Division, District]
+            include: [Division, District],
+            where: arr,
         });
         if (place_data) {
             return apiResponse.successResponseWithData(res, "Data successfully fetched.", place_data)
