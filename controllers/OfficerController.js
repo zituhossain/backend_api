7f0c4cb4c504bce3c3dc89c9103033f38aec3b89
Sonjoy Biswas <110379938+sonjoyvegans@users.noreply.github.com>
2023-02-02 20:26:52 +0600

    raw query and officer update

--- controllers/OfficerController.js
+++ controllers/OfficerController.js
@@ -1,139 +1,143 @@
 const apiResponse = require('../helpers/apiResponse');
-const {Officer} = require('../models');
+const { Officer } = require('../models');
 
-exports.createofficer = async(req,res) => {
-    if(req.file){
-        const filePath = `uploads/officer/${req.file.filename}`
-        req.body.image = filePath;
-    }
-    try{
-        if(req.body.name && req.body.email && req.body.phone && req.body.address){
-            const officer_data = await Officer.findOne({where: {name:req.body.name}})
-            if(officer_data){
-                return apiResponse.ErrorResponse(res,"NGO officer already found in database")
-            }else{
-                req.body.status = 'active';
-                await Officer.create(req.body)
-                return apiResponse.successResponse(res,"Data successfully saved.")
-            }
+exports.createofficer = async (req, res) => {
+	if (req.file) {
+		const filePath = `uploads/officer/${req.file.filename}`
+		req.body.image = filePath;
+	}
+	try {
+		if (req.body.name && req.body.email && req.body.phone && req.body.address) {
+			const officer_data = await Officer.findOne({ where: { name: req.body.name } })
+			if (officer_data) {
+				return apiResponse.ErrorResponse(res, "NGO officer already found in database")
+			} else {
+				req.body.status = 'active';
+				await Officer.create(req.body)
+				return apiResponse.successResponse(res, "Data successfully saved.")
+			}
 
-        }else{
-            return apiResponse.ErrorResponse(res,'name/address/phone/email missing')
-        }
+		} else {
+			return apiResponse.ErrorResponse(res, 'name/address/phone/email missing')
+		}
 
-    }catch(err){
-        return apiResponse.ErrorResponse(res,err.message)
-    }
+	} catch (err) {
+		return apiResponse.ErrorResponse(res, err.message)
+	}
 }
 
-exports.getallofficer = async(req,res) => {
-    try{
-        const officer_data = await Officer.findAll();
-        if(officer_data){
-            return apiResponse.successResponseWithData(res,"Data successfully fetched.",officer_data)
-        }else{
-            return apiResponse.ErrorResponse(res,"Officer table is empty.")
-        }
+exports.getallofficer = async (req, res) => {
+	try {
+		const officer_data = await Officer.findAll();
+		if (officer_data) {
+			return apiResponse.successResponseWithData(res, "Data successfully fetched.", officer_data)
+		} else {
+			return apiResponse.ErrorResponse(res, "Officer table is empty.")
+		}
 
-    }catch(err){
-        return apiResponse.ErrorResponse(res,err.message)
-    }
+	} catch (err) {
+		return apiResponse.ErrorResponse(res, err.message)
+	}
 }
 
-exports.getofficerbyid = async(req,res) => {
-    try{
-        const officer_id = req.params.id;
-        const officer_data = await Officer.findOne({where:{id: officer_id}});
-        if(officer_data){
-            return apiResponse.successResponseWithData(res,"Data successfully fetched.",officer_data)
-        }else{
-            return apiResponse.ErrorResponse(res,"No matching query found")
-        }
+exports.getofficerbyid = async (req, res) => {
+	try {
+		const officer_id = req.params.id;
+		const officer_data = await Officer.findOne({ where: { id: officer_id } });
+		if (officer_data) {
+			return apiResponse.successResponseWithData(res, "Data successfully fetched.", officer_data)
+		} else {
+			return apiResponse.ErrorResponse(res, "No matching query found")
+		}
 
-    }catch(err){
-        return apiResponse.ErrorResponse(res,err.message)
-    }
+	} catch (err) {
+		return apiResponse.ErrorResponse(res, err.message)
+	}
 }
 
-exports.deleteofficerbyid = async(req,res) => {
-    try{
-        const officer_id = req.params.id;
-        const officer_data = await Officer.findOne({where:{id: officer_id}});
-        if(officer_data){
-            await Officer.update({ status: "deleted" }, { where: { id: officer_id } });
-            return apiResponse.successResponse(res,"Data successfully deleted.")
-        }else{
-            return apiResponse.ErrorResponse(res,"No matching query found")
-        }
+exports.deleteofficerbyid = async (req, res) => {
+	try {
+		const officer_id = req.params.id;
+		const officer_data = await Officer.findOne({ where: { id: officer_id } });
+		if (officer_data) {
+			await Officer.update({ status: "deleted" }, { where: { id: officer_id } });
+			return apiResponse.successResponse(res, "Data successfully deleted.")
+		} else {
+			return apiResponse.ErrorResponse(res, "No matching query found")
+		}
 
-    }catch(err){
-        return apiResponse.ErrorResponse(res,err.message)
-    }
+	} catch (err) {
+		return apiResponse.ErrorResponse(res, err.message)
+	}
 }
 
-exports.activeofficerbyid = async(req,res) => {
-    try{
-        const officer_id = req.params.id;
-        const officer_data = await Officer.findOne({where:{id: officer_id}});
-        if(officer_data){
-            await Officer.update({ status: "active" }, { where: { id: officer_id } });
-            return apiResponse.successResponse(res,"Data successfully activated.")
-        }else{
-            return apiResponse.ErrorResponse(res,"No matching query found")
-        }
+exports.activeofficerbyid = async (req, res) => {
+	try {
+		const officer_id = req.params.id;
+		const officer_data = await Officer.findOne({ where: { id: officer_id } });
+		if (officer_data) {
+			await Officer.update({ status: "active" }, { where: { id: officer_id } });
+			return apiResponse.successResponse(res, "Data successfully activated.")
+		} else {
+			return apiResponse.ErrorResponse(res, "No matching query found")
+		}
 
-    }catch(err){
-        return apiResponse.ErrorResponse(res,err.message)
-    }
+	} catch (err) {
+		return apiResponse.ErrorResponse(res, err.message)
+	}
 }
 
-exports.updateofficerbyid = async(req,res) => {
-    if(req.file){
-        const filePath = `uploads/officer/${req.file.filename}`
-        req.body.image = filePath;
-    }
-    try{
-        const officer_id = req.params.id;
-        const officer_data = await Officer.findOne({where:{id: officer_id}});
-        if(officer_data){
-            if(req.body.name && req.body.email && req.body.phone && req.body.address){
-                await Officer.update(req.body, { where: { id: officer_id } });
-                return apiResponse.successResponse(res,"Data successfully updated.")
-            }else{
-                return apiResponse.ErrorResponse(res,'name/address/phone/email missing')
-            }
-        }else{
-            return apiResponse.ErrorResponse(res,"No matching query found")
-        }
+exports.updateofficerbyid = async (req, res) => {
+	if (req.file) {
+		const filePath = `uploads/officer/${req.file.filename}`
+		req.body.image = filePath;
+	}
+	try {
+		const officer_id = req.params.id;
+		const officer_data = await Officer.findOne({ where: { id: officer_id } });
+		if (officer_data) {
+			if (req.body.name && req.body.email && req.body.phone && req.body.address) {
+				if (req.body.image === 'null') {
+					await Officer.update({ name: req.body.name, email: req.body.email, phone: req.body.phone, address: req.body.address }, { where: { id: officer_id } });
+				} else {
+					await Officer.update(req.body, { where: { id: officer_id } });
+				}
+				return apiResponse.successResponse(res, "Data successfully updated.")
+			} else {
+				return apiResponse.ErrorResponse(res, 'name/address/phone/email missing')
+			}
+		} else {
+			return apiResponse.ErrorResponse(res, "No matching query found")
+		}
 
-    }catch(err){
-        return apiResponse.ErrorResponse(res,err.message)
-    }
+	} catch (err) {
+		return apiResponse.ErrorResponse(res, err.message)
+	}
 }
 
-exports.getactiveofficer = async(req,res) => {
-    try{
-        const officer_data = await Officer.findAll({where:{status:'active'}});
-        if(officer_data){
-            return apiResponse.successResponseWithData(res,"Data successfully fetched.",officer_data)
-        }else{
-            return apiResponse.ErrorResponse(res,"No active officer found.")
-        }
+exports.getactiveofficer = async (req, res) => {
+	try {
+		const officer_data = await Officer.findAll({ where: { status: 'active' } });
+		if (officer_data) {
+			return apiResponse.successResponseWithData(res, "Data successfully fetched.", officer_data)
+		} else {
+			return apiResponse.ErrorResponse(res, "No active officer found.")
+		}
 
-    }catch(err){
-        return apiResponse.ErrorResponse(res,err.message)
-    }
+	} catch (err) {
+		return apiResponse.ErrorResponse(res, err.message)
+	}
 }
-exports.getdeletedofficer = async(req,res) => {
-    try{
-        const officer_data = await Officer.findAll({where:{status:'deleted'}});
-        if(officer_data){
-            return apiResponse.successResponseWithData(res,"Data successfully fetched.",officer_data)
-        }else{
-            return apiResponse.ErrorResponse(res,"No deactive officer found.")
-        }
+exports.getdeletedofficer = async (req, res) => {
+	try {
+		const officer_data = await Officer.findAll({ where: { status: 'deleted' } });
+		if (officer_data) {
+			return apiResponse.successResponseWithData(res, "Data successfully fetched.", officer_data)
+		} else {
+			return apiResponse.ErrorResponse(res, "No deactive officer found.")
+		}
 
-    }catch(err){
-        return apiResponse.ErrorResponse(res,err.message)
-    }
+	} catch (err) {
+		return apiResponse.ErrorResponse(res, err.message)
+	}
 }
--- controllers/PlaceController.js
+++ controllers/PlaceController.js
@@ -3,7 +3,7 @@ const apiResponse = require('../helpers/apiResponse');
 const checkUserRoleByPlace = require('./globalController');
 const { ngoServedPercentByPlace , ngoJotAddIntoPlace} = require('../validator/place');
 
-const { Place,ngo_jots, Division, District, ngo_category_b, ngo_served_percent_by_palces, ngo_jot_add_into_places, year_place_ngo_officer, Ngo, Officer } = require('../models');
+const { Place,ngo_jots, Division, District, ngo_category_b, ngo_served_percent_by_palces, ngo_jot_add_into_places, year_place_ngo_officer, Ngo, Officer, sequelize } = require('../models');
 const { where } = require('sequelize');
 exports.getallPlace = async (req, res) => {
     try {
@@ -162,8 +162,9 @@ exports.getDistrictByDivision = async (req, res) => {
     try {
         const id = req.params.id
         const district_data = await District.findAll({ where: { division_id: id } });
-        if (district_data) {
-            return apiResponse.successResponseWithData(res, "Data successfully fetched.", district_data)
+		const [results, metadata]  = await sequelize.query(`select Places.name,Officers.name as officer_name,Places.id as place_id,Officers.image from Places LEFT JOIN year_place_ngo_officers ypno on ypno.place_id = Places.id LEFT JOIN Officers on Officers.id = ypno.officer_id where Places.division_id = ${id} GROUP BY Places.id`);
+        if (results) {
+            return apiResponse.successResponseWithData(res, "Data successfully fetched.", results)
         } else {
             return apiResponse.ErrorResponse(res, "District table is empty.")
         }
