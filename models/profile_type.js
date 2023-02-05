b976f46ad87ee0533b038310508aea730bb4c287
Sonjoy Biswas <sonjoywc@gmail.com>
2023-02-05 12:36:32 +0600

    profile type

---
+++ models/profile_type.js
@@ -0,0 +1,24 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Profile_type extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Profile_type.init({
+    type: DataTypes.STRING,
+    sort: DataTypes.NUMBER
+  }, {
+    sequelize,
+    modelName: 'Profile_type',
+  });
+  return Profile_type;
+};
