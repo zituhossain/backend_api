86cf43936046bd1c32328d159f88b97e16b40e41
Mahidul Moon <49182474+mahidulmoon@users.noreply.github.com>
2023-02-19 00:09:00 +0600

    report 2

--- controllers/FilterController.js
+++ controllers/FilterController.js
@@ -97,3 +97,49 @@ exports.finalReportGenerate = async(req,res) => {
         return apiResponse.ErrorResponse(res,"No data found")
     }
 }
+exports.finalReportGenerateDoubleNGO = async(req,res) => {
+    let query = ''
+    if(req.body.year_id != ''){
+        const get_year = await years.findOne({where:{id:req.body.year_id}})
+        if(query.includes('where')){
+            query += ` and year = '${get_year.name}'`
+        }else{
+            query += ` where year = '${get_year.name}'`
+        }
+        
+    }
+
+    if(req.body.division_id != ''){
+        const get_division = await Division.findOne({where:{id:req.body.division_id}})
+        if(query.includes('where')){
+            query += ` and division_name = '${get_division.name_bg}'`
+        }else{
+            query += ` where division_name = '${get_division.name_bg}'`
+        }
+        
+    }
+    if(req.body.district_id != ''){
+        const get_district = await District.findOne({where:{id:req.body.district_id}})
+        if(query.includes('where')){
+            query += ` and district_name = '${get_district.name_bg}'`
+        }else{
+            query += ` where district_name = '${get_district.name_bg}'`
+        }
+        
+    }
+    if(req.body.place_id != ''){
+        const get_place = await Place.findOne({where:{id:req.body.place_id}})
+        if(query.includes('where')){
+            query += ` and place_name = '${get_place.name}'`
+        }else{
+            query += ` where place_name = '${get_place.name}'`
+        }
+        
+    }
+    const [alldata, metadata] = await sequelize.query(`SELECT Ngo_place_info.*,(select Officers.name from year_place_ngo_officers LEFT JOIN Officers on Officers.id = year_place_ngo_officers.officer_id LEFT JOIN years on years.id = year_place_ngo_officers.year_id where years.bn_name = year(curdate()) and year_place_ngo_officers.place_id = Ngo_place_info.place_id and year_place_ngo_officers.ngo_id = ${req.body.ngo_id}) as ngo_officer_one, (select Officers.name from year_place_ngo_officers LEFT JOIN Officers on Officers.id = year_place_ngo_officers.officer_id LEFT JOIN years on years.id = year_place_ngo_officers.year_id where years.bn_name = year(curdate()) and year_place_ngo_officers.place_id = Ngo_place_info.place_id and year_place_ngo_officers.ngo_id = ${req.body.ngo_id2}) as ngo_officer_two FROM Ngo_place_info` + query + ` GROUP BY officer_name`);
+    if(alldata.length > 0){
+        return apiResponse.successResponseWithData(res,"all_data fetch successfully.",alldata)
+    }else{
+        return apiResponse.ErrorResponse(res,"No data found")
+    }
+}
