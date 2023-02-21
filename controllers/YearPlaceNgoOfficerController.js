0cbc939157c606a93700194255c266f003aec147
Mahidul Moon <49182474+mahidulmoon@users.noreply.github.com>
2023-02-21 22:15:28 +0600

    year place ngo officer update

--- controllers/YearPlaceNgoOfficerController.js
+++ controllers/YearPlaceNgoOfficerController.js
@@ -205,14 +205,15 @@ exports.updateoveralltitlebyid = async (req, res) => {
                 const headingsList = req.body.headingsList;
                 const headingsValueList = req.body.headingsValueList;
                 headingsList.length > 0 && headingsList.map(async (res, index) => {
+                    let get_desc = generateHash(headingsValueList[index]?.headings_value ? headingsValueList[index]?.headings_value : '');
                     const description = {
                         // ypno_id: condition_id,                        
                         officer_id: req.body.officer_id,
                         year_id: req.body.year_id,
                         heading_id: res.id,
-                        desc: headingsValueList[index]?.headings_value ? headingsValueList[index]?.headings_value : '',
+                        desc: get_desc,
                     }
-                    console.log(description);
+                    // console.log(description);
                     await officers_heading_description.create(description);
 
                 })
