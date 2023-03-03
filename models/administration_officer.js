699c2a6f5b136b98cf42ad6a44a6a10b24cf01fa
zituhossain5 <121731101+zituhossain5@users.noreply.github.com>
2023-03-03 12:46:25 +0600

    add field in admin officer table

--- models/administration_officer.js
+++ models/administration_officer.js
@@ -1,68 +1,74 @@
 'use strict';
-const {
-  Model
-} = require('sequelize');
-const PROTECTED_ATTRIBUTES = ['district_id', 'division_id']
+const { Model } = require('sequelize');
+const PROTECTED_ATTRIBUTES = ['district_id', 'division_id'];
 
 module.exports = (sequelize, DataTypes) => {
-  class Administration_officer extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
-    }
-    // toJSON () {
-    //   // hide protected fields
-    //   let attributes = Object.assign({}, this.get())
-    //   for (let a of PROTECTED_ATTRIBUTES) {
-    //     delete attributes[a]
-    //   }
-    //   return attributes
-    // }
-  }
-  Administration_officer.init({
-    place_id: DataTypes.INTEGER,
-    administration_office_id: DataTypes.INTEGER,
-    district_id: DataTypes.INTEGER,
-    division_id: DataTypes.INTEGER,
-    ordering: DataTypes.INTEGER,
-    name: DataTypes.STRING,
-    email: DataTypes.STRING,
-    phone: DataTypes.STRING,
-    designation: DataTypes.STRING,
-    joining_date: DataTypes.STRING,
-    batch: DataTypes.INTEGER,
-    ability: DataTypes.STRING,
-    honesty: DataTypes.STRING,
-    qualification: DataTypes.STRING,
-    comments: DataTypes.TEXT,
-    created_by: DataTypes.INTEGER,
-    updated_by: DataTypes.INTEGER,
-    filename: DataTypes.STRING,
-  }, {
-    sequelize,
-    modelName: 'Administration_officer',
-  });
-  Administration_officer.associate = models => {
-    Administration_officer.belongsTo(models.Administration_office, {
-      foreignKey: 'administration_office_id',
-    });
-    Administration_officer.belongsTo(models.District, {
-      foreignKey: 'district_id',
-    });
-    Administration_officer.belongsTo(models.Division, {
-      foreignKey: 'division_id',
-    });
-    Administration_officer.belongsTo(models.Place, {
-      foreignKey: 'place_id',
-    });
-    Administration_officer.belongsTo(models.Administration_officer_type, {
-      targetKey: 'id',
-      foreignKey: 'designation',
-    });
-  }
-  return Administration_officer;
+	class Administration_officer extends Model {
+		/**
+		 * Helper method for defining associations.
+		 * This method is not a part of Sequelize lifecycle.
+		 * The `models/index` file will call this method automatically.
+		 */
+		static associate(models) {
+			// define association here
+		}
+		// toJSON () {
+		//   // hide protected fields
+		//   let attributes = Object.assign({}, this.get())
+		//   for (let a of PROTECTED_ATTRIBUTES) {
+		//     delete attributes[a]
+		//   }
+		//   return attributes
+		// }
+	}
+	Administration_officer.init(
+		{
+			place_id: DataTypes.INTEGER,
+			administration_office_id: DataTypes.INTEGER,
+			district_id: DataTypes.INTEGER,
+			division_id: DataTypes.INTEGER,
+			office_id: DataTypes.STRING,
+			ordering: DataTypes.INTEGER,
+			name: DataTypes.STRING,
+			email: DataTypes.STRING,
+			phone: DataTypes.STRING,
+			address: DataTypes.STRING,
+			designation: DataTypes.STRING,
+			joining_date: DataTypes.STRING,
+			batch: DataTypes.INTEGER,
+			ability: DataTypes.STRING,
+			honesty: DataTypes.STRING,
+			qualification: DataTypes.STRING,
+			educational_institute: DataTypes.STRING,
+			old_work_place: DataTypes.STRING,
+			other_info: DataTypes.STRING,
+			comments: DataTypes.TEXT,
+			created_by: DataTypes.INTEGER,
+			updated_by: DataTypes.INTEGER,
+			filename: DataTypes.STRING,
+		},
+		{
+			sequelize,
+			modelName: 'Administration_officer',
+		}
+	);
+	Administration_officer.associate = (models) => {
+		Administration_officer.belongsTo(models.Administration_office, {
+			foreignKey: 'administration_office_id',
+		});
+		Administration_officer.belongsTo(models.District, {
+			foreignKey: 'district_id',
+		});
+		Administration_officer.belongsTo(models.Division, {
+			foreignKey: 'division_id',
+		});
+		Administration_officer.belongsTo(models.Place, {
+			foreignKey: 'place_id',
+		});
+		Administration_officer.belongsTo(models.Administration_officer_type, {
+			targetKey: 'id',
+			foreignKey: 'designation',
+		});
+	};
+	return Administration_officer;
 };
