e32732540efd0a8e73918fa1bdb306d9a5b9907d
Sonjoy Biswas <sonjoywc@gmail.com>
2023-03-06 01:58:50 +0600

    population

--- routes/population_year_place.js
+++ routes/population_year_place.js
@@ -8,6 +8,8 @@ router.get('/all_population_year_place/:id',auth, PopulationYearPlaceController.
 router.get('/all_population_year_place_by_place/:place',auth, PopulationYearPlaceController.getbyYear);
 router.get('/all_population_year_place_by_district/:disId',auth, PopulationYearPlaceController.getbyDisId);
 router.get('/all_population_year_place_by_division/:divId',auth, PopulationYearPlaceController.getbyDivId);
+router.get('/all_population_year_place_by_id/:placeId',auth, PopulationYearPlaceController.getbyPlaceId);
+router.get('/all_population_minority_by_id/:placeId',auth, PopulationYearPlaceController.getMinoritybyPlaceId);
 
 router.post('/create_population_year_place',auth, PopulationYearPlaceController.create);
 router.post('/update_population_year_place/:id',auth, PopulationYearPlaceController.updatebyid);
