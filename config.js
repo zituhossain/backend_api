74d70862ff607766bf1189abb0488652c225db3f
Mahidul Moon <49182474+mahidulmoon@users.noreply.github.com>
2023-03-06 13:32:39 +0600

    cutome mongo connection successful

--- config.js
+++ config.js
@@ -9,6 +9,7 @@ module.exports = {
 	},
 	// base_dir:'/Users/md.mahidulislam/Desktop/Full Stack/Office/GitHub/NGO/justtest/',
 	base_dir: __dirname + '/',
+	mongo_db_url: 'mongodb://localhost:27017/zvgy_ngo',
 	database: {
 		host: 'zvgy.com',
 		port: '3306',
