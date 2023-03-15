81e97e1324a86e60b4155929d7a5356bb530e9c4
Sonjoy Biswas <sonjoywc@gmail.com>
2023-03-16 01:41:48 +0600

    96

--- controllers/FilterController.js
+++ controllers/FilterController.js
@@ -79,9 +79,9 @@ exports.finalReportGenerate = async(req,res) => {
     }
     let custome_query = '';
     if(req.body.ngo_id === ''){ 
-        custome_query = `,(select officers.name from year_place_ngo_officers LEFT JOIN officers on officers.id = year_place_ngo_officers.officer_id LEFT JOIN years on years.id = year_place_ngo_officers.year_id where years.name = (select Max(name) from years) and year_place_ngo_officers.place_id = ngo_place_info.place_id and year_place_ngo_officers.ngo_id = 1 and year_place_ngo_officers.status=1) as ngo_officer_one`
+        custome_query = `,(select officers.name from year_place_ngo_officers LEFT JOIN officers on officers.id = year_place_ngo_officers.officer_id LEFT JOIN years on years.id = year_place_ngo_officers.year_id where years.name = (select Max(name) from years) and year_place_ngo_officers.place_id = ngo_place_info.place_id and year_place_ngo_officers.ngo_id = 1 and year_place_ngo_officers.status=1 limit 1) as ngo_officer_one`
     }else{
-        custome_query = `,(select officers.name from year_place_ngo_officers LEFT JOIN officers on officers.id = year_place_ngo_officers.officer_id LEFT JOIN years on years.id = year_place_ngo_officers.year_id where years.name = (select Max(name) from years) and year_place_ngo_officers.place_id = ngo_place_info.place_id and year_place_ngo_officers.ngo_id = ${req.body.ngo_id} and year_place_ngo_officers.status=1) as ngo_officer_one`
+        custome_query = `,(select officers.name from year_place_ngo_officers LEFT JOIN officers on officers.id = year_place_ngo_officers.officer_id LEFT JOIN years on years.id = year_place_ngo_officers.year_id where years.name = (select Max(name) from years) and year_place_ngo_officers.place_id = ngo_place_info.place_id and year_place_ngo_officers.ngo_id = ${req.body.ngo_id} and year_place_ngo_officers.status=1 limit 1) as ngo_officer_one`
     } 
     if(req.body.ngo_id !== ''){        
         const get_ngo = await Ngo.findOne({where:{id:req.body.ngo_id}})
