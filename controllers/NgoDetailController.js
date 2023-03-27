fe75edcae1e1f1052624297b8951859862f9ed53
zituhossain5 <isminhaz3@gmail.com>
2023-03-27 16:45:09 +0600

    ordering in ngo info pointwise

--- controllers/NgoDetailController.js
+++ controllers/NgoDetailController.js
@@ -38,7 +38,11 @@ exports.getlocalinfluencerbyid = async (req, res) => {
 exports.getngodetailwiseinfobyplaceid = async (req, res) => {
     try {
         const influencer_id = req.params.placeid;
-        const influencer_data = await ngo_details_info.findAll({ include: [{ model: ngo_details_info_point_wise, where: {place_id: influencer_id},required:false }] });
+        const influencer_data = await ngo_details_info.findAll({
+            order:[
+                ['id', 'ASC'],
+               [[ngo_details_info_point_wise, "view_order", "ASC"]]], 
+               include: [{ model: ngo_details_info_point_wise, where: {place_id: influencer_id},required:false }] });
         if (influencer_data) {
             return apiResponse.successResponseWithData(res, "Data successfully fetched.", influencer_data)
         } else {
