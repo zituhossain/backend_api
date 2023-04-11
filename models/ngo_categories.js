a7ebaad290f57c52012254f35492639b10bd8eb0
zituhossain5 <121731101+zituhossain5@users.noreply.github.com>
2023-04-11 14:48:46 +0600

    add order in categoryBController getAll API

--- models/ngo_categories.js
+++ models/ngo_categories.js
@@ -1,28 +1,30 @@
 'use strict';
-const {
-  Model
-} = require('sequelize');
+const { Model } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class Ngo extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
-    }
-  }
-  Ngo.init({
-    
-    name:DataTypes.STRING,
-    short_name:DataTypes.STRING,
-    color_code:DataTypes.STRING,
-    values:DataTypes.STRING,
-  }, {
-    timestamps:false,
-    sequelize,
-    modelName: 'ngo_categories',
-  });
-  return Ngo;
+	class Ngo extends Model {
+		/**
+		 * Helper method for defining associations.
+		 * This method is not a part of Sequelize lifecycle.
+		 * The `models/index` file will call this method automatically.
+		 */
+		static associate(models) {
+			// define association here
+		}
+	}
+	Ngo.init(
+		{
+			name: DataTypes.STRING,
+			short_name: DataTypes.STRING,
+			color_code: DataTypes.STRING,
+			values: DataTypes.STRING,
+			type: DataTypes.TINYINT,
+			view_order: { type: DataTypes.INTEGER, allowNull: true },
+		},
+		{
+			timestamps: false,
+			sequelize,
+			modelName: 'ngo_categories',
+		}
+	);
+	return Ngo;
 };
