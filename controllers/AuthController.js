f3bd668fe2e33106af5ca2859195b9443cf7bf1d
zituhossain5 <121731101+zituhossain5@users.noreply.github.com>
2023-05-02 16:05:05 +0600

    create api for update user

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,11 +1,17 @@
-const { User,Login_attempt,blacklist_ip,Previlege_table,Previlege_url } = require('../models');
+const {
+	User,
+	Login_attempt,
+	blacklist_ip,
+	Previlege_table,
+	Previlege_url,
+} = require('../models');
 //helper file to prepare responses.
 const apiResponse = require('../helpers/apiResponse');
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt');
 const IP = require('ip');
 let ipAddress = IP.address();
-const { Op } = require("sequelize");
+const { Op } = require('sequelize');
 /**
  * User registration.
  *
@@ -19,47 +25,94 @@ function generatePassword(value) {
 	return bcrypt.hashSync(value, salt);
 	// const saltRounds = 10;
 	// return bcrypt.hashSync(value, saltRounds);
-
 }
 
 function dycryptandmatch(value, hashStoredInDB) {
-	const result = bcrypt.compare(value, hashStoredInDB)
-	return result
-
+	const result = bcrypt.compare(value, hashStoredInDB);
+	return result;
 }
 exports.register = async (req, res) => {
 	try {
 		// const jane = await User.create({ firstName: "Jane", lastName: "Doe" });
-		const { firstname, lastname, password1, password2, office_id, phone, role_id } = req.body;
+		const {
+			username,
+			firstname,
+			lastname,
+			password1,
+			password2,
+			office_id,
+			phone,
+			role_id,
+		} = req.body;
 		if (password1 === password2) {
-			return apiResponse.ErrorResponse(
-				res,
-				'Password can not be same',
-			);
+			return apiResponse.ErrorResponse(res, 'Password can not be same');
 		} else {
-			req.body.username = firstname + lastname;
+			// req.body.username = firstname + lastname;
 			req.body.status = 'pending';
-			const user = await User.findOne({ where: { username: req.body.username } });
+			const user = await User.findOne({
+				where: { username: req.body.username },
+			});
 			if (user) {
-
-				return apiResponse.ErrorResponse(
-					res,
-					'Duplicate user found',
-				);
+				return apiResponse.ErrorResponse(res, 'Duplicate user found');
 			} else {
 				req.body.password1 = generatePassword(password1);
 				req.body.password2 = generatePassword(password2);
 				await User.create(req.body);
-				return apiResponse.successResponse(
-					res,
-					'User Creation Success.',
-				);
+				return apiResponse.successResponse(res, 'User Creation Success.');
+			}
+		}
+	} catch (err) {
+		//throw error in json response with status 500.
+		console.log(err);
+		return apiResponse.ErrorResponse(res, err);
+	}
+};
+
+exports.updateUser = async (req, res) => {
+	try {
+		const userId = req.params.id;
+		const {
+			username,
+			firstname,
+			lastname,
+			office_id,
+			phone,
+			role_id,
+			status,
+			password1,
+			password2,
+		} = req.body;
+
+		const user = await User.findByPk(userId);
+
+		if (!user) {
+			return apiResponse.ErrorResponse(res, 'User not found');
+		}
+
+		user.username = username;
+		user.firstname = firstname;
+		user.lastname = lastname;
+		user.password1 = password1;
+		user.password2 = password2;
+		user.office_id = office_id;
+		user.phone = phone;
+		user.role_id = role_id;
+		user.status = status;
+
+		if (password1 && password2) {
+			if (password1 === password2) {
+				return apiResponse.ErrorResponse(res, 'Password can not be same');
+			} else {
+				user.password1 = generatePassword(password1);
+				user.password2 = generatePassword(password2);
 			}
 		}
 
+		await user.save();
+
+		return apiResponse.successResponse(res, 'User Update Success.');
 	} catch (err) {
-		//throw error in json response with status 500.
-		console.log(err)
+		console.log(err);
 		return apiResponse.ErrorResponse(res, err);
 	}
 };
@@ -78,7 +131,7 @@ exports.login = async (req, res) => {
 		const user = await User.findOne({ where: { username: req.body.username } });
 		if (user) {
 			if (password2 === '') {
-				const passwordmatch = await dycryptandmatch(password1, user.password1)
+				const passwordmatch = await dycryptandmatch(password1, user.password1);
 				if (passwordmatch) {
 					const data = {
 						error: false,
@@ -92,10 +145,13 @@ exports.login = async (req, res) => {
 					);
 				}
 			} else {
-				if(user.status==='active'){
-					const passwordmatch2 = await dycryptandmatch(password2, user.password2)
+				if (user.status === 'active') {
+					const passwordmatch2 = await dycryptandmatch(
+						password2,
+						user.password2
+					);
 					if (passwordmatch2) {
-						var previlege_table_data = []
+						var previlege_table_data = [];
 						// if(user.role_id === 1){
 						// 	previlege_table_data = await Previlege_url.findAll()
 						// }else{
@@ -104,16 +160,16 @@ exports.login = async (req, res) => {
 						// 		where:{user_role_id: user.role_id}
 						// 	})
 						// }
-						let final_array = []
-						for(i=0;i<previlege_table_data.length;i++){
-							final_array.push(previlege_table_data[i].name)
+						let final_array = [];
+						for (i = 0; i < previlege_table_data.length; i++) {
+							final_array.push(previlege_table_data[i].name);
 						}
 						let userData = {
 							_id: user.id,
 							username: user.username,
 							phone: user.phone,
 							role: user.role_id,
-							privilege_url: final_array
+							privilege_url: final_array,
 						};
 						const jwtPayload = userData;
 						const jwtData = {
@@ -121,64 +177,112 @@ exports.login = async (req, res) => {
 						};
 						const secret = process.env.JWT_SECRET;
 						const token = jwt.sign(jwtPayload, secret, jwtData);
-						try{
-							ipAddress = req.headers['x-forwarded-for'] || req.headers['x-real-ip'] || req.socket.remoteAddress
-							let attempt_data = await Login_attempt.findOne({where:{user_ip: ipAddress}})
-							if(attempt_data){
-								await Login_attempt.update({attempt:1,status:'end'},{where:{user_ip:ipAddress,attempt:1,status:'running'}})
-								await Login_attempt.update({attempt:1,status:'end'},{where:{user_ip:ipAddress,status:'error'}})
-							}else{
-								await Login_attempt.create({user_id:user.id,user_ip:ipAddress,attempt:1,status:'running'})
+						try {
+							ipAddress =
+								req.headers['x-forwarded-for'] ||
+								req.headers['x-real-ip'] ||
+								req.socket.remoteAddress;
+							let attempt_data = await Login_attempt.findOne({
+								where: { user_ip: ipAddress },
+							});
+							if (attempt_data) {
+								await Login_attempt.update(
+									{ attempt: 1, status: 'end' },
+									{
+										where: {
+											user_ip: ipAddress,
+											attempt: 1,
+											status: 'running',
+										},
+									}
+								);
+								await Login_attempt.update(
+									{ attempt: 1, status: 'end' },
+									{ where: { user_ip: ipAddress, status: 'error' } }
+								);
+							} else {
+								await Login_attempt.create({
+									user_id: user.id,
+									user_ip: ipAddress,
+									attempt: 1,
+									status: 'running',
+								});
 							}
-
-						}catch(err){
-							console.log("login error: ",err.message)
+						} catch (err) {
+							console.log('login error: ', err.message);
 						}
 						return apiResponse.successResponseWithDataNToken(
 							res,
 							'login successfull.',
 							user,
 							token
-						)
+						);
 					} else {
-						try{
-							ipAddress = req.headers['x-forwarded-for'] || req.headers['x-real-ip'] || req.socket.remoteAddress
-							let attempt_data = await Login_attempt.findOne({where:{user_ip: ipAddress}})
-							if(attempt_data){
+						try {
+							ipAddress =
+								req.headers['x-forwarded-for'] ||
+								req.headers['x-real-ip'] ||
+								req.socket.remoteAddress;
+							let attempt_data = await Login_attempt.findOne({
+								where: { user_ip: ipAddress },
+							});
+							if (attempt_data) {
 								let value = true;
-								if(attempt_data.attempt === 1 && attempt_data.status === 'end'){
-									await Login_attempt.update({attempt:1,status:'error'},{where:{id:attempt_data.id}})
+								if (
+									attempt_data.attempt === 1 &&
+									attempt_data.status === 'end'
+								) {
+									await Login_attempt.update(
+										{ attempt: 1, status: 'error' },
+										{ where: { id: attempt_data.id } }
+									);
 									value = false;
 								}
-								if(attempt_data.attempt === 2){
-									await Login_attempt.update({attempt:attempt_data.attempt+1},{where:{id:attempt_data.id,status:'error'}})
-									await User.update({ status: "pending" }, { where: { id: user.id } });
-									await blacklist_ip.create({user_id:user.id,user_ip:ipAddress,status: 'block'});
-								}else{
-									if(value){
-										await Login_attempt.update({attempt:attempt_data.attempt+1},{where:{id:attempt_data.id,status:'error'}})
+								if (attempt_data.attempt === 2) {
+									await Login_attempt.update(
+										{ attempt: attempt_data.attempt + 1 },
+										{ where: { id: attempt_data.id, status: 'error' } }
+									);
+									await User.update(
+										{ status: 'pending' },
+										{ where: { id: user.id } }
+									);
+									await blacklist_ip.create({
+										user_id: user.id,
+										user_ip: ipAddress,
+										status: 'block',
+									});
+								} else {
+									if (value) {
+										await Login_attempt.update(
+											{ attempt: attempt_data.attempt + 1 },
+											{ where: { id: attempt_data.id, status: 'error' } }
+										);
 									}
 								}
-							}else{
-								await Login_attempt.create({user_id:user.id,user_ip:ipAddress,attempt:1,status:'error'})
+							} else {
+								await Login_attempt.create({
+									user_id: user.id,
+									user_ip: ipAddress,
+									attempt: 1,
+									status: 'error',
+								});
 							}
-
-						}catch(err){
-							console.log("login error: ",err.message)
+						} catch (err) {
+							console.log('login error: ', err.message);
 						}
 						return apiResponse.unauthorizedResponse(
 							res,
 							'password did not matched'
 						);
 					}
-				}else{
+				} else {
 					return apiResponse.unauthorizedResponse(
 						res,
 						'user is not activated by admin'
 					);
 				}
 			}
-
 		} else {
 			return apiResponse.unauthorizedResponse(
 				res,
@@ -190,7 +294,6 @@ exports.login = async (req, res) => {
 	}
 };
 
-
-exports.deactivateuser = async(req,res) => {
-	return apiResponse.successResponse(res,"user deactivate successfull.")
-}
+exports.deactivateuser = async (req, res) => {
+	return apiResponse.successResponse(res, 'user deactivate successfull.');
+};
--- routes/auth.js
+++ routes/auth.js
@@ -5,5 +5,6 @@ const router = express.Router();
 
 router.post('/register', AuthController.register);
 router.post('/login', AuthController.login);
+router.put('/update_user/:id', AuthController.updateUser);
 
 module.exports = router;
