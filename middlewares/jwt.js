73ef4d4ad3c1244ae3929f47dcbbe6e6c6e21c0c
zituhossain5 <121731101+zituhossain5@users.noreply.github.com>
2023-06-08 15:53:33 +0600

    Update: years model add new type field

--- middlewares/jwt.js
+++ middlewares/jwt.js
@@ -2,24 +2,29 @@ const jwt = require('jsonwebtoken');
 const secret = process.env.JWT_SECRET;
 const apiResponse = require('../helpers/apiResponse');
 const IP = require('ip');
-const { blacklist_ip, User, Previlege_url, Previlege_table } = require('../models');
-const { Op } = require("sequelize");
-const { createLogger, format, transports } = require("winston");
+const {
+	blacklist_ip,
+	User,
+	Previlege_url,
+	Previlege_table,
+} = require('../models');
+const { Op } = require('sequelize');
+const { createLogger, format, transports } = require('winston');
 const date = new Date();
 let day = date.getDate();
 let month = date.getMonth() + 1;
 let year = date.getFullYear();
 let currentDate = `${day}-${month}-${year}`;
 
-const userModel = require("../models/mongo_models");
+const userModel = require('../models/mongo_models');
 
 var logger = createLogger({
 	format: format.combine(format.timestamp(), format.json()),
 	transports: [
 		new transports.Console({}),
-		new transports.File({ filename: "log/"+currentDate+".log" }),
+		new transports.File({ filename: 'log/' + currentDate + '.log' }),
 	],
-  });
+});
 
 const authenticate = (roles = []) => {
 	if (typeof roles === 'string') {
@@ -70,37 +75,65 @@ const authenticate = (roles = []) => {
 // }
 
 function checkurl(value) {
-  const segments = value.split("/");
-  
-  if (segments.length >= 3 && parseInt(segments[segments.length - 2]) && parseInt(segments[segments.length - 1])) {
-    segments[segments.length - 2] = ":condition";
-    segments[segments.length - 1] = ":id";
-    value = segments.join("/");
-    return { status: true, url: value };
-  } else if (parseInt(segments[segments.length - 1])) {
-    segments[segments.length - 1] = ":id";
-    value = segments.join("/");
-    return { status: true, url: value };
-  } else {
-    value = segments.join("/");
-    return { status: false, url: value };
-  }
+	const segments = value.split('/');
+	console.log('segment', segments);
+	console.log('segment2', segments[segments.length - 2]);
+
+	if (
+		(segments.length >= 3 && parseInt(segments[segments.length - 2])) ||
+		(segments[segments.length - 2] && parseInt(segments[segments.length - 1]))
+	) {
+		segments[segments.length - 2] = ':condition';
+		segments[segments.length - 1] = ':id';
+		value = segments.join('/');
+		return { status: true, url: value };
+	} else if (parseInt(segments[segments.length - 1])) {
+		segments[segments.length - 1] = ':id';
+		value = segments.join('/');
+		return { status: true, url: value };
+	} else {
+		value = segments.join('/');
+		return { status: false, url: value };
+	}
 }
 
+// function checkurl(value) {
+// 	const segments = value.split('/');
+// 	const conditionSegment = segments[segments.length - 2];
+// 	const idSegment = segments[segments.length - 1];
+
+// 	const conditionRegex = /^[a-zA-Z]+$/; // Regular expression to match alphabetic characters
+// 	console.log('seg=', segments[segments.length - 2]);
+// 	if (
+// 		segments.length >= 3 &&
+// 		conditionRegex.test(conditionSegment) &&
+// 		/^\d+$/.test(idSegment)
+// 	) {
+// 		segments[segments.length - 2] = ':condition';
+
+// 		segments[segments.length - 1] = ':id';
+// 	} else if (/^\d+$/.test(idSegment)) {
+// 		segments[segments.length - 1] = ':id';
+// 	}
+
+// 	value = segments.join('/');
+// 	return { status: true, url: value };
+// }
+
 const save_to_mongo = async (body) => {
-    const user = new userModel(body);
+	const user = new userModel(body);
 	// const users = await userModel.find({});
 	// console.log(users)
-  
-    try {
-      await user.save();
-	//   console.log("mongo saved")
-    } catch (error) {
-		console.log("mongo save error: ",error.message)
-    }
+
+	try {
+		await user.save();
+		//   console.log("mongo saved")
+	} catch (error) {
+		console.log('mongo save error: ', error.message);
+	}
 };
 
-function createLog(data){
+function createLog(data) {
 	logger.info(data);
 	// save_to_mongo(data);
 }
@@ -108,7 +141,10 @@ function createLog(data){
 
 module.exports = async (req, res, next) => {
 	let ipAddress = IP.address();
-	ipAddress = req.headers['x-forwarded-for'] || req.headers['x-real-ip'] || req.socket.remoteAddress
+	ipAddress =
+		req.headers['x-forwarded-for'] ||
+		req.headers['x-real-ip'] ||
+		req.socket.remoteAddress;
 	// console.log("ip: ",ipAddress);
 	const token = req.headers.authorization.split(' ')[1];
 	const decodedToken = jwt.verify(token, secret);
@@ -116,54 +152,75 @@ module.exports = async (req, res, next) => {
 	const logdata = {
 		api_route: req.originalUrl,
 		method: req.method,
-		body: req.body && Object.keys(req.body).length === 0 ? "" : req.body,
+		body: req.body && Object.keys(req.body).length === 0 ? '' : req.body,
 		user_id: userId,
-		ip: ipAddress
-	}
-	createLog(logdata)
+		ip: ipAddress,
+	};
+	createLog(logdata);
 	try {
 		// console.log("aaaaaaaaaaaaaaaaaaaaaaaa", req.originalUrl)
 		let validate_url = checkurl(req.originalUrl);
-		let finalurl = req.originalUrl
-		if(validate_url.status){
-			finalurl = validate_url.url
+		let finalurl = req.originalUrl;
+		if (validate_url.status) {
+			finalurl = validate_url.url;
 		}
 		if (userId && decodedToken.phone === '') {
 			throw 'Invalid user ID';
 		} else {
-			const user_data = await User.findOne({ where: { id: userId } })
+			const user_data = await User.findOne({ where: { id: userId } });
 			if (user_data.status !== 'active') {
-				return apiResponse.unauthorizedResponse(res, "User is not activated to authorize.")
+				return apiResponse.unauthorizedResponse(
+					res,
+					'User is not activated to authorize.'
+				);
 			}
 			if (user_data.role_id && user_data.role_id === 1) {
 				// console.log("super user");
 				next();
 			} else {
-				let block_list_check = await blacklist_ip.findAll({ where: { [Op.and]: [{ status: 'block' }, { user_ip: ipAddress }] } })
+				let block_list_check = await blacklist_ip.findAll({
+					where: { [Op.and]: [{ status: 'block' }, { user_ip: ipAddress }] },
+				});
 				if (block_list_check.length > 0) {
-					return apiResponse.unauthorizedResponse(res, 'You are not authorized.Your Ip is blocked');
+					return apiResponse.unauthorizedResponse(
+						res,
+						'You are not authorized.Your Ip is blocked'
+					);
 				} else {
 					// next();
 					// const url_data = await Previlege_url.findOne({ where: { url: req.originalUrl } })
-					const url_data = await Previlege_url.findOne({ where: { url: finalurl } })
+					const url_data = await Previlege_url.findOne({
+						where: { url: finalurl },
+					});
 					if (url_data) {
-						const url_table_data = await Previlege_table.findOne({ where: { user_role_id: user_data.role_id, previlege_url_id: url_data.id } })
+						const url_table_data = await Previlege_table.findOne({
+							where: {
+								user_role_id: user_data.role_id,
+								previlege_url_id: url_data.id,
+							},
+						});
 						if (url_table_data) {
 							next();
 						} else {
-							console.log('url name - ',url_data?.name);
-							console.log('url - ',url_data?.url);
-							console.log('user_role_id - ',user_data.role_id);
-							console.log('url_id - ',url_data.id);
+							console.log('url name - ', url_data?.name);
+							console.log('url - ', url_data?.url);
+							console.log('user_role_id - ', user_data.role_id);
+							console.log('url_id - ', url_data.id);
 							console.log('------------jwt.js 111----------');
 							console.log(finalurl);
-							return apiResponse.unauthorizedResponse(res, "You have no access on this url")
+							return apiResponse.unauthorizedResponse(
+								res,
+								'You have no access on this url'
+							);
 						}
 					} else {
 						console.log('------------jwt.js 222----------');
 						console.log(finalurl);
-						console.log("aaaaaaaaaaaaaaaaaaaaaaaa", req.originalUrl)
-						return apiResponse.unauthorizedResponse(res, "You have no access on this url")
+						console.log('aaaaaaaaaaaaaaaaaaaaaaaa', req.originalUrl);
+						return apiResponse.unauthorizedResponse(
+							res,
+							'You have no access on this url'
+						);
 					}
 				}
 			}
--- models/years.js
+++ models/years.js
@@ -1,29 +1,31 @@
 'use strict';
-const {
-  Model
-} = require('sequelize');
+const { Model } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class year_place_ngo_officer extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
-    }
-  }
-  year_place_ngo_officer.init({
-    name: DataTypes.INTEGER,
-    bn_name: DataTypes.STRING,
-    bn_term: DataTypes.STRING,
-  }, {
-    sequelize,
-    modelName: 'years',
-    timestamps:false
-  });
+	class year_place_ngo_officer extends Model {
+		/**
+		 * Helper method for defining associations.
+		 * This method is not a part of Sequelize lifecycle.
+		 * The `models/index` file will call this method automatically.
+		 */
+		static associate(models) {
+			// define association here
+		}
+	}
+	year_place_ngo_officer.init(
+		{
+			name: DataTypes.INTEGER,
+			bn_name: DataTypes.STRING,
+			bn_term: DataTypes.STRING,
+			type: DataTypes.INTEGER,
+		},
+		{
+			sequelize,
+			modelName: 'years',
+			timestamps: false,
+		}
+	);
 
-  year_place_ngo_officer.associate = models => { }
+	year_place_ngo_officer.associate = (models) => {};
 
-  return year_place_ngo_officer;
+	return year_place_ngo_officer;
 };
